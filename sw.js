57920fe89b3db4edb6c54bea29ecc93116647a8f
zipher759-EOD <zipher759@aol.com>
2026-07-22 10:14:58 -0700

    Add Between Us family game

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='between-us-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
